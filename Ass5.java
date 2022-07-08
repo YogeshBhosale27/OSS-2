76d0f745589d072c050dd8c87f2602bb28d4f96e
saurabhchavan09 <93591769+saurabhchavan09@users.noreply.github.com>
2022-07-08 15:44:47 +0530

    Add files via upload

---
+++ Ass5.java
@@ -0,0 +1,13 @@
+public class Ass5
+{
+	public static void main(String []args)
+	{
+		int i,j=5;
+		for(i=5;i>0;i--)
+		{
+			System.out.println();
+			for(j=5;j>=i;j--)
+			System.out.print(j);
+		}
+	}
+}
